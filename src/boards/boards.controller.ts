3246e5e12398652f3fdbd0349d04472bb1483450
HamSangeok <hamsoo159@naver.com>
2024-06-18 00:10:50 +0900

    CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,36 @@
+import { CreateBoardDto } from 'src/dto/create-board.dto';
+import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardService: BoardsService) {}
+    constructor(private boardService: BoardsService) {}
+
+    // handler
+    @Get()
+    getAllBoard(): Board[] {
+      return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+      return this.boardService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+      return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+      return this.boardService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus) {
+      return this.boardService.updateBoardStatus(id, status);
+    }
+  
 }
